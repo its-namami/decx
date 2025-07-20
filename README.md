b132dcfc9e7622a38c33cf0f7b1e5e7fe76a7667
its-namami <164639260+its-namami@users.noreply.github.com>
2025-07-21 01:34:50 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,116 @@
+![GitHub repo size](https://img.shields.io/github/repo-size/its-namami/mafs-js) ![GitHub License](https://img.shields.io/github/license/its-namami/mafs-js) ![GitHub Issues](https://img.shields.io/github/issues/its-namami/mafs-js)
+
+<p align="left">
+  <img src='https://raw.githubusercontent.com/its-namami/mafs-js/main/assets/media/images/docs/mafs-js-logo.webp' alt='Mafs.js Logo' width='200'/>
+</p>
+
+# Mafs.js
+
+A custom-built JavaScript math engine focused on precision, clarity, and full control.
+No dependencies. No borrowed logic. Just pure vanilla JS, crafted from scratch.
+
+## 🚀 Features
+
+- Arbitrary-precision number handling (BigInt-friendly)
+- Core arithmetic: addition, subtraction, multiplication, division
+- Square root (in progress)
+- Planned: logarithms, exponentiation, trigonometry, parsing, memory stack
+- Eventually: graphical *extension* (plotting via canvas)
+
+## 💡 Philosophy
+
+Mafs is not meant to be a competitor to Decimal.js or Math.js.
+It’s an **exercise in reinventing the wheel** — fully self-developed, no lookups, no libraries.
+Every feature is designed, debugged, and refined from scratch as a personal learning journey.
+
+## 🧠 Why?
+
+I started this project as part of [The Odin Project](https://www.theodinproject.com/), but it grew beyond a simple calculator.
+This is a math engine and it's teaching me how math, precision, and JavaScript really work under the hood.
+Every feature is born from the challenge of solving it unaided — this project is less about speed and more about understanding the why behind every result.
+
+## 📦 Cloning the project
+
+This project isn't meant for production yet, but feel free to clone it:
+
+If you’d like to experiment or contribute, cloning is the way to go. No build steps required.
+
+```zsh
+git clone https://github.com/its-namami/mafs-js.git
+```
+
+## Method Input Expectations:
+
+### Mafs.number(input):
+
+- Accepts string or number.
+
+### Mafs.multiply(input):
+
+- string (recommended): Expected format for precise calculations.
+
+- number (not recommended): May lead to precision issues.
+
+- bigint: Supported, but the .value getter will return a string in decimal form when needed (e.g., for non-integer results).
+
+### Mafs.number(x).sqrt():
+
+- Does not expect any input parameters.
+
+## Example Usage
+
+To create a new instance of the `Mafs` class, use the static `Mafs.number()` method.
+- **Please refrain from using `new Mafs()` directly**, as it may not handle certain edge cases correctly.
+
+The `Mafs.number()` method accepts a number, a bigint or a string representing a number.
+- **Please refrain from inputting number type**: Mafs.number(3.14) → leads to warning (and subtle bugs).
+
+```js
+import Mafs from './path/to/mafs.js';
+
+// Create a Mafs number from a string
+const num1 = Mafs.number('-1.23');
+
+// Example: Division
+const num2 = '3.501';
+const result = num1.divide(num2).value; // returns '-0.351328191945158'
+
+// Don't do this: Multiplication with a warning about number-type input
+Mafs.number('2.25').multiply(0.000001).value; // Warning: Prefer string input for precision, returns '0.00000225'
+
+// Example: Addition with high precision
+const preciseSum = Mafs.number('0.000000000000000000000000000000001')
+  .add(Mafs.number('0.000000000000000000000000000000002'))
+  .value; // Returns '0.000000000000000000000000000000003'
+```
+
+## Roadmap
+For more detailed progress and future plans, see the [Mafs.js Project Board](https://github.com/users/its-namami/projects/3) or explore the [issues](https://github.com/its-namami/mafs-js/issues)
+
+### Math Engine Core
+
+- [x] Mafs Class
+
+- [x] Addition, Subtraction, etc.
+
+- [ ] Square Root
+
+- [ ] Logarithms
+
+- [ ] Exponentiation
+
+### Extended Features
+
+- [ ] Expression Parser
+
+- [ ] Constants (π, e)
+
+- [ ] Memory/Registers
+
+- [ ] Graphical Plotting (Canvas)
+
+### Smart Input
+
+- [ ] Word-based Input Parsing
+
+Built solo with ❤️ by [its-namami](https://github.com/its-namami).
---
+++ mafs.js
@@ -0,0 +1,260 @@
+class Mafs {
+  #isNegative;
+  #absDigits;
+  #decimalLength;
+  #rawDigits;
+
+  constructor(number) {
+    let numberString = number.toString();
+
+    if (numberString.indexOf('e') !== -1) {
+      numberString = Mafs.#deScientify(numberString);
+    }
+
+    numberString = Mafs.#trimZeroes(numberString);
+
+    let decimalPointPosition = numberString.indexOf('.') + 1;
+
+    this.#absDigits = BigInt(numberString.replace(/-|\./g, ''));
+    this.#isNegative = numberString[0] === '-';
+    this.#rawDigits = (this.#isNegative ? this.#absDigits * -1n : this.#absDigits);
+
+    if (decimalPointPosition === 0) {
+      this.#decimalLength = 0;
+    } else {
+      this.#decimalLength = numberString.length - decimalPointPosition;
+    }
+  }
+
+  get value() {
+    return Mafs.#normalize(this.#rawDigits.toString(), this.#decimalLength);
+  }
+
+  sqrt() {
+    const number = this.#getCopy();
+
+    const precision = 17n;
+    let seeking = number.#rawDigits * 10n ** precision;
+    let lowerLimit = 1n * 10n ** (precision);
+    let upperLimit = number.#rawDigits * 10n ** (precision);
+    let tolerance = 10n ** BigInt(number.#rawDigits.toString().length);
+
+    const calculate = {
+      get average() {
+        return (lowerLimit + upperLimit) / 2n;
+      },
+      get averageSquared() {
+        return (this.average ** 2n) / 10n ** precision;
+      }
+    };
+
+    if (number.#absDigits === 0n) return 0;
+
+    if (number.value[0] !== '0' && !number.#isNegative) {
+      let i = 0;
+
+      while (i < 500) {
+        i++;
+
+        if (calculate.averageSquared >= seeking && calculate.averageSquared - tolerance < seeking) {
+          break;
+        } else {
+          if (calculate.averageSquared > seeking) {
+            upperLimit = calculate.average;
+          } else {
+            lowerLimit = calculate.average;
+          }
+        }
+      }
+
+      return Mafs.number(Mafs.#normalize(calculate.average, calculate.average.toString().length - Math.round(number.#absDigits.length / 2)));
+
+    } else if (!number.#isNegative) {
+      seeking = seeking * 10n ** BigInt(number.#decimalLength);
+      upperLimit = seeking;
+      let i = 0;
+
+      while (i < 500) {
+        i++;
+
+        if (calculate.averageSquared >= seeking && calculate.averageSquared - tolerance < seeking) {
+          break;
+        } else {
+          if (calculate.averageSquared > seeking) {
+            upperLimit = calculate.average;
+          } else {
+            lowerLimit = calculate.average;
+          }
+        }
+      }
+
+      return Mafs.number(Mafs.#normalize(calculate.average, calculate.average.toString().length + number.#absDigits.length / 2 - 1));
+    } else {
+      return Mafs.#error('negSqrt');
+    }
+  }
+
+  add(secondNumber) {
+    const firstNumber = this.#getCopy();
+
+    if (secondNumber instanceof Mafs === false) secondNumber = Mafs.number(secondNumber);
+
+    const maxLen = Math.max(firstNumber.#absDigits.length, secondNumber.#absDigits.length);
+    const maxDecimalLen = Math.max(firstNumber.#decimalLength, secondNumber.#decimalLength);
+    const firstNumberValue = firstNumber.#growDecimal(maxDecimalLen);
+    const secondNumberValue = secondNumber.#growDecimal(maxDecimalLen);
+    const numberResult = firstNumberValue + secondNumberValue;
+
+    return Mafs.number(Mafs.#normalize(numberResult, maxDecimalLen));
+  }
+
+  multiply(secondNumber) {
+    const firstNumber = this.#getCopy();
+
+    if (secondNumber instanceof Mafs === false) secondNumber = Mafs.number(secondNumber);
+
+    const sumLen = firstNumber.#decimalLength + secondNumber.#decimalLength;
+    const numberResult = firstNumber.#rawDigits * secondNumber.#rawDigits;
+
+    return Mafs.number(Mafs.#normalize(numberResult, sumLen));
+  }
+
+  divide(secondNumber) {
+    const firstNumber = this.#getCopy();
+
+    if (secondNumber instanceof Mafs === false) secondNumber = Mafs.number(secondNumber);
+
+    const maxLen = Math.max(firstNumber.#absDigits.length, secondNumber.#absDigits.length);
+    const maxDecimalLen = Math.max(firstNumber.#decimalLength, secondNumber.#decimalLength);
+    let firstNumberValue = firstNumber.#growDecimal(maxDecimalLen);
+    const secondNumberValue = secondNumber.#growDecimal(maxDecimalLen);
+
+    if (secondNumberValue === 0n) return Mafs.#error('divideZero');
+
+    let decimals = 0;
+
+    for (let i = 0; i < 15; i++) {
+      if (firstNumberValue % secondNumberValue !== 0n) {
+        firstNumberValue *= 10n;
+        decimals++;
+      } else {
+        break;
+      }
+    }
+
+    const result = (firstNumberValue / secondNumberValue).toString();
+
+    return Mafs.number(Mafs.#normalize(result, decimals));
+  }
+
+  static number(input) {
+    Mafs.#validate(input);
+
+    if (typeof input === "number") {
+      Mafs.#warning('inputNumber');
+      return new Mafs(input.toString());
+    } else {
+      return new Mafs(input);
+    }
+  }
+
+  #getCopy() {
+    return Mafs.number(this.value);
+  }
+
+  #growDecimal(scale) {
+    const decimalsNeeded = scale - this.#decimalLength;
+
+    if (decimalsNeeded < 0) return Mafs.#error('badScale', decimalsNeeded);
+
+    return this.#rawDigits * 10n ** BigInt(decimalsNeeded);
+  }
+
+  static #validate(number) {
+    switch (typeof number) {
+      case 'string':
+        break;
+      case 'number':
+        number = number.toString();
+        break;
+      case 'bigint':
+        number = number.toString();
+        break;
+      default:
+        return Mafs.#error('badNum', number);
+    }
+
+    const validNumberRegex = /^[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?$/;
+
+    if (!validNumberRegex.test(number)) return Mafs.#error('badNum', number);
+  }
+
+  static #error(error = 'default', info = undefined) {
+    switch (error) {
+      case 'badNum':
+        throw new Error (`\nIncorrect number: '${info}' is not a valid number!`);
+      case 'badScale':
+        throw new Error (`\nCannot have less than 0 decimals: (${info})!`);
+      case 'divideZero':
+        throw new Error ('\nCannot divide by zero!');
+      case 'negSqrt':
+        throw new Error ('\nSquare root of a negative number is imaginary!');
+      case 'default':
+        throw new Error ("\nFatal error, that's all we know...");
+    }
+  }
+
+  static #warning(warning = 'default', info = undefined) {
+    switch (warning) {
+      case 'sqrtIsOverItterated':
+        console.warn(`Sqrt calculation went above planned iterations: forcefully returning current close-enough sqrt`);
+        break;
+      case 'inputNumber':
+        console.warn(`Don't use numbers: Prefer string or bigint input for precision`);
+        break;
+      default:
+        console.warn("There's some warning, that's all we know...");
+    }
+  }
+
+  static #normalize(digits, decimals) {
+    const negativeSign = (digits.toString()[0] === '-' ? '-' : '');
+    digits = (negativeSign === '-' ? digits.toString().slice(1) : digits.toString());
+    const digitsLength = digits.length - (digits[0] === '-' ? 1 : 0);
+
+    if (decimals === 0 || decimals === undefined) {
+      return negativeSign + digits;
+    } else if (digitsLength > decimals) {
+      return negativeSign + digits.slice(0, digits.length - decimals) + '.' + digits.slice(digits.length - decimals);
+    } else {
+      return negativeSign + '0.' + '0'.repeat(decimals - digitsLength) + digits;
+    }
+  }
+
+  static #trimZeroes = str => {
+    const dotIndex = str.indexOf('.');
+    str = str.replace(/^0+/g, '');
+
+    if (str.indexOf('.') !== -1) {
+      str = str.replace(/0+$/g, '');
+
+      if (str.at(-1) === '.') str = str.slice(0, -1);
+      if (str.at(0) === '.') str = '0' + str;
+    }
+
+    return str === '' ? '0' : str;
+  }
+
+  static #deScientify = str => {
+    const split = str.split('e');
+    const decimalFactor = (
+      split[1][0] === '-'
+        ? '0.' + '0'.repeat(+split[1].slice(1) - 1) + '1'
+        : '1' + '0'.repeat(+split[1])
+    )
+
+    return Mafs.number(split[0]).multiply(decimalFactor).value;
+  }
+}
+
+export default Mafs;
